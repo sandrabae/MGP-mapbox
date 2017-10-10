3853f81c82a09988811158d2fbfa651072ff94c5
K-Dasu <keshavdasu@yahoo.com>
2017-10-10 14:25:31 -0700

    Drawing Polygons
    Polygonal representation for makers

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,9 +16,9 @@ loadingMap = function(stores) {
         // style URL
         style: 'mapbox://styles/mapbox/light-v9',
         // initial position in [long, lat] format
-        center: [-74.50, 40],
+        center: [-103.913, 18.527],
         // initial zoom
-        zoom: 8,
+        zoom: 5,
         scrollZoom: true
     });
 
@@ -31,115 +31,139 @@ loadingMap = function(stores) {
             "type": "geojson",
             "data": stores
         });
+
+        map.addLayer({
+            "id": "places-boundary",
+            "type": "fill",
+            "source": "places",
+            "paint": {
+                "fill-color": "#888888",
+                "fill-opacity": 0.4
+            },
+            "filter": ["==", "$type", "Polygon"]
+        });
+
+        // Change the cursor to a pointer when the mouse is over the states layer.
+        map.on('mouseenter', 'places-boundary', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+
+        map.on('click', 'places-boundary', function (e) {
+            //Catch the data for this polygon and forward it to the stream-map 
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.name)
+                .addTo(map);
+        });
         // Initialize the list
-        buildLocationList(stores);
+        // buildLocationList(stores);
 
     });
 
-    // This is where your interactions with the symbol layer used to be
-    // Now you have interactions with DOM markers instead
-    stores.features.forEach(function(marker, i) {
-        // Create an img element for the marker
-        var el = document.createElement('div');
-        el.id = "marker-" + i;
-        el.className = 'marker';
-        // Add markers to the map at all points
-        new mapboxgl.Marker(el, {
-                offset: [-28, -46]
-            })
-            .setLngLat(marker.geometry.coordinates)
-            .addTo(map);
+    // // This is where your interactions with the symbol layer used to be
+    // // Now you have interactions with DOM markers instead
+    // stores.features.forEach(function(marker, i) {
+    //     // Create an img element for the marker
+    //     var el = document.createElement('div');
+    //     el.id = "marker-" + i;
+    //     el.className = 'marker';
+    //     // Add markers to the map at all points
+    //     new mapboxgl.Marker(el, {
+    //             offset: [-28, -46]
+    //         })
+    //         .setLngLat(marker.geometry.coordinates)
+    //         .addTo(map);
 
-        el.addEventListener('click', function(e) {
-            // 1. Fly to the point
-            flyToStore(marker);
+    //     el.addEventListener('click', function(e) {
+    //         // 1. Fly to the point
+    //         flyToStore(marker);
 
-            // 2. Close all other popups and display popup for clicked store
-            createPopUp(marker);
+    //         // 2. Close all other popups and display popup for clicked store
+    //         createPopUp(marker);
 
-            // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-            var activeItem = document.getElementsByClassName('active');
+    //         // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+    //         var activeItem = document.getElementsByClassName('active');
 
-            e.stopPropagation();
-            if (activeItem[0]) {
-                activeItem[0].classList.remove('active');
-            }
+    //         e.stopPropagation();
+    //         if (activeItem[0]) {
+    //             activeItem[0].classList.remove('active');
+    //         }
 
-            var listing = document.getElementById('listing-' + i);
-            listing.classList.add('active');
+    //         var listing = document.getElementById('listing-' + i);
+    //         listing.classList.add('active');
 
-        });
-    });
+    //     });
+    // });
 
-    function flyToStore(currentFeature) {
-        map.flyTo({
-            center: currentFeature.geometry.coordinates,
-            zoom: 15
-        });
-    }
+    // function flyToStore(currentFeature) {
+    //     map.flyTo({
+    //         center: currentFeature.geometry.coordinates,
+    //         zoom: 15
+    //     });
+    // }
 
-    function createPopUp(currentFeature) {
-        var popUps = document.getElementsByClassName('mapboxgl-popup');
-        if (popUps[0]) popUps[0].remove();
-
-
-        var popup = new mapboxgl.Popup({
-                closeOnClick: false
-            })
-            .setLngLat(currentFeature.geometry.coordinates)
-            .setHTML('<h3>Sweetgreen</h3>' +
-                '<h4>' + currentFeature.properties.address + '</h4>')
-            .addTo(map);
-    }
+    // function createPopUp(currentFeature) {
+    //     var popUps = document.getElementsByClassName('mapboxgl-popup');
+    //     if (popUps[0]) popUps[0].remove();
 
 
-    function buildLocationList(data) {
-        for (i = 0; i < data.features.length; i++) {
-            var currentFeature = data.features[i];
-            var prop = currentFeature.properties;
-
-			console.log(currentFeature);
-			console.log(prop);
-
-            var listings = document.getElementById('listings');
-            var listing = listings.appendChild(document.createElement('div'));
-            listing.className = 'item';
-            listing.id = "listing-" + i;
-
-            var link = listing.appendChild(document.createElement('a'));
-            link.href = '#';
-            link.className = 'title';
-            link.dataPosition = i;
-            link.innerHTML = prop.address;
-
-            var details = listing.appendChild(document.createElement('div'));
-            details.innerHTML = prop.city;
-            if (prop.phone) {
-                details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-            }
+    //     var popup = new mapboxgl.Popup({
+    //             closeOnClick: false
+    //         })
+    //         .setLngLat(currentFeature.geometry.coordinates)
+    //         .setHTML('<h3>Cast</h3>' +
+    //             '<h4> # Samples: ' + currentFeature.properties.Samples + '</br>' + currentFeature.geometry.coordinates.toString() + '</h4>')
+    //         .addTo(map);
+    // }
 
 
+    // function buildLocationList(data) {
+    //     for (i = 0; i < data.features.length; i++) {
+    //         var currentFeature = data.features[i];
+    //         var prop = currentFeature.properties;
 
-            link.addEventListener('click', function(e) {
-                // Update the currentFeature to the store associated with the clicked link
-                var clickedListing = data.features[this.dataPosition];
+	
+    //         var listings = document.getElementById('listings');
+    //         var listing = listings.appendChild(document.createElement('div'));
+    //         listing.className = 'item';
+    //         listing.id = "listing-" + i;
 
-                // 1. Fly to the point
-                flyToStore(clickedListing);
+    //         var link = listing.appendChild(document.createElement('a'));
 
-                // 2. Close all other popups and display popup for clicked store
-                createPopUp(clickedListing);
+    //         link.href = '#';
+    //         link.className = 'title';
+    //         link.dataPosition = i;
+    //         link.innerHTML = currentFeature.geometry.coordinates.toString();
 
-                // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-                var activeItem = document.getElementsByClassName('active');
+    //         var details = listing.appendChild(document.createElement('div'));
+    //         var date =  prop.Date[0];
+    //         details.innerHTML = "# of Samples: " + prop.Samples;
+    //         if (prop.Date) {
+    //             details.innerHTML += ' </br> Date: ' + date;
+    //         }
 
-                if (activeItem[0]) {
-                    activeItem[0].classList.remove('active');
-                }
-                this.parentNode.classList.add('active');
 
-            });
-        }
-    }
+
+    //         link.addEventListener('click', function(e) {
+    //             // Update the currentFeature to the store associated with the clicked link
+    //             var clickedListing = data.features[this.dataPosition];
+
+    //             // 1. Fly to the point
+    //             flyToStore(clickedListing);
+
+    //             // 2. Close all other popups and display popup for clicked store
+    //             createPopUp(clickedListing);
+
+    //             // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+    //             var activeItem = document.getElementsByClassName('active');
+
+    //             if (activeItem[0]) {
+    //                 activeItem[0].classList.remove('active');
+    //             }
+    //             this.parentNode.classList.add('active');
+
+    //         });
+    //     }
+    // }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,172 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'yas' });
 });
 
+
+function aggregateLocation(data){
+    var features = []
+    var location = {};
+
+    data.forEach(function(d){
+        var longAndLatArray = d["geometry"]["coordinates"].toString();
+        if(longAndLatArray in location){
+            var date = d["properties"]["date"];
+            var dateArray = location[longAndLatArray]["Date"];
+            if(dateArray.indexOf(date) < 0){location[longAndLatArray]["Date"].push(date);}
+            location[longAndLatArray]["Samples"] = location[longAndLatArray]["Samples"] + 1;
+        }else{
+            var date = d["properties"]["date"];
+            location[longAndLatArray] = { Date:[date], Samples: 1};
+        }
+        
+    });
+
+    
+    var polygons = createPolygons(location);
+
+    polygons.forEach(function(d){ features.push(d);});
+
+    return features;
+}
+
+function markers(data){
+    var solution = [];
+
+    for(var key in data){
+        var coordinates = key.split(',');
+        var object = { type: "Feature", 
+            geometry:{"coordinates": coordinates, type: "Point"}, 
+            properties: {Samples: location[key]["Samples"], Date: location[key]["Date"] }
+        };
+
+        solution.push(object);
+    }
+
+    return solution;
+}
+
+function createPolygons(data){
+    var US = [];
+    var US_Data = [];
+    var Greenland = [];
+    var GreenData = [];
+    var polygons = [];
+
+    //TODO: method for grouping a set coordinates to be then turned into polygons
+    for(var key in data){
+        var stringCoordinates = key.split(',');
+        var coordinates = [0,0];
+        coordinates[0] = parseFloat(stringCoordinates[0]);
+        coordinates[1] = parseFloat(stringCoordinates[1]);
+
+        if(coordinates[0] > -156  && coordinates[0] < -70){
+            //United states related check if between -156 && -70
+            US.push(coordinates);
+            US_Data.push(data[key]);
+        }else{
+            //Greenland check if between -6 && 39
+            Greenland.push(coordinates);
+            GreenData.push(data[key]);
+        }
+    }
+
+    var USPoly = JarvisMarch(US);
+    var GreenlandPoly =  JarvisMarch(Greenland);
+
+    var polys = [USPoly, GreenlandPoly];
+    var names = ["America W", "Greenland"];
+    polys.forEach(function(poly, i){
+        var object = { type: "Feature", geometry:{"coordinates": [poly], type: "Polygon"}, properties: {name: names[i]}};
+        polygons.push(object);
+    });
+
+    return polygons;
+}
+
+function JarvisMarch(data){
+    var sortedData = Array.from(data);
+    sortedData.sort(function(a,b){ return a[0] - b[0];}); //sort by ascending
+
+    var pointOnHull = sortedData[0]; //left most point
+    var pointsOnHull = [pointOnHull];
+    var angleAbsolutePrev = 0;
+    var displacement = [0,0];
+
+    while(pointOnHull != pointsOnHull[0] ||  pointsOnHull.length == 1){
+        var minAngleRelativeSoFar = Number.POSITIVE_INFINITY;
+        var pointWithMinAngleRelativeSoFar = null;
+        var i = 0;
+        for(;i < data.length; i++){
+            var pointCandidate = data[i];
+
+            //pointCandidate - pointOnHull
+            var x = pointCandidate[0] - pointOnHull[0];
+            var y =  pointCandidate[1] - pointOnHull[1];
+
+            displacement[0] = x; 
+            displacement[1] = y;
+
+            //magnitude
+            var distanceFromPointOnHullToCandidate = magnitude(displacement);
+
+            if(distanceFromPointOnHullToCandidate != 0){
+                var angleAbsolute = angleInCycles(displacement);
+                var angleRelativeToHullEdge = angleAbsolute - angleAbsolutePrev;
+
+                if(angleRelativeToHullEdge < 0){ angleRelativeToHullEdge += 1;}
+
+                if(angleRelativeToHullEdge < minAngleRelativeSoFar ){
+                    if (angleRelativeToHullEdge == minAngleRelativeSoFar){
+                        displacement[0] = pointWithMinAngleRelativeSoFar[0] -  pointOnHull[0];
+                        displacement[1] = pointWithMinAngleRelativeSoFar[1] -  pointOnHull[1];
+
+                        var distancePrev = magnitude(displacement);
+                        if (distanceFromPointOnHullToCandidate < distancePrev){
+                            minAngleRelativeSoFar = angleRelativeToHullEdge;
+                            pointWithMinAngleRelativeSoFar =  pointCandidate;
+                        }
+
+                    }else{
+                        minAngleRelativeSoFar = angleRelativeToHullEdge;
+                        pointWithMinAngleRelativeSoFar = pointCandidate;
+                    }
+                }
+
+            }
+        }
+
+        pointOnHull = pointWithMinAngleRelativeSoFar;
+        pointsOnHull.push(pointOnHull);
+        angleAbsolutePrev += minAngleRelativeSoFar;
+    }
+
+    return pointsOnHull;
+}
+
+function angleInCycles(point){
+    var solution = Math.atan2(point[1], point[0]) / (2 * Math.PI);
+
+    if(solution < 0){
+        solution += 1;
+    }
+    return solution;
+}
+
+function magnitude(point){
+    return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+}
+
+
 router.post('/ajax', function(req, res) {
-    var result = require('../public/javascripts/test2'),
+    var result = require('../public/javascripts/test2'), //54k rows
      body = req.body,
      start = body["start"],
      end = body["end"],
      features = result["features"];
+    console.log(features.length); 
 
-    var featuresSlice = features.slice(start,end);
-    var geoJSON = {"type": "FeatureCollection", "features": featuresSlice}
+    // var featuresSlice = features.slice(start,end);
+    var slimmedSlice = aggregateLocation(features);
+    var geoJSON = {"type": "FeatureCollection", "features": slimmedSlice}
 
     if(req.xhr || req.accepts('json,html')==='json'){
         res.json({success: true , data :geoJSON });
