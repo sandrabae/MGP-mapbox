c50e11e6988df566b8b3e4a5fd96259c7195da50
K-Dasu <keshavdasu@yahoo.com>
2017-10-11 03:10:33 -0700

    Placeholder Stream Graph...

--- public/javascripts/comparisons.js
+++ public/javascripts/comparisons.js
@@ -1,173 +1,75 @@
-initializeChart = function(){
-  chart("../data/R-Data.json", "orange");
+var n = 20, // number of layers
+    m = 200, // number of samples per layer
+    stack = d3.stack().keys(d3.range(n).map(function (d) { return "layer"+d; })).offset(d3.stackOffsetWiggle);
 
-  var datearray = [];
-  var colorrange = [];
+// Create empty data structures
+var matrix0 = d3.range(m).map(function (d) { return { x:d }; });
+var matrix1 = d3.range(m).map(function (d) { return { x:d }; });
 
+// Fill them with random data
+d3.range(n).map(function(d) { bumpLayer(m, matrix0, d); });
+d3.range(n).map(function(d) { bumpLayer(m, matrix1, d); });
 
-  function chart(csvpath, color) {
+var layers0 = stack(matrix0),
+    layers1 = stack(matrix1);
 
-    if (color == "blue") {
-      colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
-    }
-    else if (color == "pink") {
-      colorrange = ["#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA", "#F1EEF6"];
-    }
-    else if (color == "orange") {
-      colorrange = ["#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E", "#FEF0D9"];
-    }
-    strokecolor = colorrange[0];
+var width = 900,
+    height = 500;
 
-    var format = d3.timeFormat("%YYYY/%d/%m");
+var x = d3.scaleLinear()
+    .domain([0, m - 1])
+    .range([0, width]);
 
-    var margin = {top: 20, right: 40, bottom: 30, left: 30};
-    var width = document.body.clientWidth - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
+var y = d3.scaleLinear()
+    .domain([d3.min(layers0.concat(layers1), function(layer) { return d3.min(layer, function(d) { return d[0]; }); }), d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d[1]; }); })])
+    .range([height, 0]);
 
-    var tooltip = d3.select("body")
-    .append("div")
-    .attr("class", "remove")
-    .style("position", "absolute")
-    .style("z-index", "20")
-    .style("visibility", "hidden")
-    .style("top", "30px")
-    .style("left", "55px");
+var color = d3.scaleLinear()
+    .range(["#aad", "#556"]);
 
-    var x = d3.scaleBand().range([0, width]);
-
-    var y = d3.scaleBand().range([height-10, 0]);
-
-    var z = d3.scalePoint().range(colorrange);
-
-    var xAxis =  d3.axisBottom(x).ticks(d3.timeWeek);
-
-    var yAxis = d3.axisLeft(y);
-    var yAxisr = d3.axisLeft(y);
-
-    
-
-    var area = d3.area()
-    .x(function(d) {  return x(d.data.date); })
+var area = d3.area()
+    .x(function(d,i) { return x(d.data.x); })
     .y0(function(d) { return y(d[0]); })
     .y1(function(d) { return y(d[1]); });
 
-    var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-    var graph = d3.json(csvpath, function(data) {
+svg.selectAll("path")
+    .data(layers0)
+  .enter().append("path")
+    .attr("d", area)
+    .style("fill", function() { return color(Math.random()); });
 
-      var uniqueMicrobe = {};
-
-      data.forEach(function(d){
-        if(!(d['ITIS TSN'] in uniqueMicrobe )){
-            uniqueMicrobe[d['ITIS TSN']] = 1;
-        }else{
-            uniqueMicrobe[d['ITIS TSN']] += 1;
-        }
-
-      });
-
-      console.log(uniqueMicrobe);
-
-      var stack = d3.stack().order(d3.stackOrderNone).offset(d3.stackOffsetWiggle).keys(Object.keys(uniqueMicrobe));
-      var nest = d3.nest().key(function(d) { return d['ITIS TSN']; }).entries(data);
-
-      var layers = stack(nest);
-      console.log(nest);
-      console.log(layers);
-
-      x.domain(d3.extent(data, function(d) { return d.date; }));
-      y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
-
-      svg.selectAll(".layer")
-      .data(layers)
-      .enter().append("path")
-      .attr("class", "layer")
-      .attr("d", function(d) { return area(d); })
-      .style("fill", function(d, i) { return z(i); });
-
-
-      svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-      svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + width + ", 0)")
-      .call(yAxisr);
-
-      svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-      svg.selectAll(".layer")
-      .attr("opacity", 1)
-      .on("mouseover", function(d, i) {
-        svg.selectAll(".layer").transition()
-        .duration(250)
-        .attr("opacity", function(d, j) {
-          return j != i ? 0.6 : 1;
-        })})
-
-      .on("mousemove", function(d, i) {
-        mousex = d3.mouse(this);
-        mousex = mousex[0];
-        var invertedx = x.invert(mousex);
-        invertedx = invertedx.getMonth() + invertedx.getDate();
-        var selected = (d.values);
-        for (var k = 0; k < selected.length; k++) {
-          datearray[k] = selected[k].date
-          datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
-        }
-
-        mousedate = datearray.indexOf(invertedx);
-        pro = d.values[mousedate].value;
-
-        d3.select(this)
-        .classed("hover", true)
-        .attr("stroke", strokecolor)
-        .attr("stroke-width", "0.5px"), 
-        tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
-
+function transition() {
+  d3.selectAll("path")
+      .data(function() {
+        var d = layers1;
+        layers1 = layers0;
+        return layers0 = d;
       })
-      .on("mouseout", function(d, i) {
-       svg.selectAll(".layer")
-       .transition()
-       .duration(250)
-       .attr("opacity", "1");
-       d3.select(this)
-       .classed("hover", false)
-       .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
-     })
+    .transition()
+      .duration(2500)
+      .attr("d", area);
+}
 
-      var vertical = d3.select(".chart")
-      .append("div")
-      .attr("class", "remove")
-      .style("position", "absolute")
-      .style("z-index", "19")
-      .style("width", "1px")
-      .style("height", "380px")
-      .style("top", "10px")
-      .style("bottom", "30px")
-      .style("left", "0px")
-      .style("background", "#fff");
+// Inspired by Lee Byron's test data generator.
+function bumpLayer(n, matrix, layer) {
 
-      d3.select(".chart")
-      .on("mousemove", function(){  
-       mousex = d3.mouse(this);
-       mousex = mousex[0] + 5;
-       vertical.style("left", mousex + "px" )})
-      .on("mouseover", function(){  
-       mousex = d3.mouse(this);
-       mousex = mousex[0] + 5;
-       vertical.style("left", mousex + "px")});
-    });
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < n; i++) {
+      var w = (i / n - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
   }
 
-};
-
-initializeChart();
+  var a = [], i;
+  var i;
+  for (i = 0; i < n; ++i) a[i] = 0;
+  for (i = 0; i < 5; ++i) bump(a);
+  return a.forEach(function(d, i) { matrix[i]["layer"+layer]=Math.max(0, d)+1; });
+}
