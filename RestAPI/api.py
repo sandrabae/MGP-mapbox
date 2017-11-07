a1c1280cbc97140d9bbc21b9b7af41cc6fcf3bc7
K-Dasu <keshavdasu@yahoo.com>
2017-11-07 14:39:10 -0800

    one file for now

--- RestAPI/api.py
+++ RestAPI/api.py
@@ -2,9 +2,8 @@ from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
+from bson.json_util import dumps
 import requests
-
-from classes.time_range import TimeRange 
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +11,23 @@ api = Api(app)
 
 app.config['MONGO2_DBNAME'] = 'test' 
 mongo = PyMongo(app, config_prefix='MONGO2')
+
+
+class TimeRange(Resource):
+
+	# def get handles a post request
+    def get(self):
+    	test = mongo.db.noaa.find({'Year':'2006'})
+    	l_test = list(test)
+
+    	solution = {'data': dumps(l_test) }
+    	return solution
+
+    # def post handles a post request
+    def post(self):
+    	print(request.data)
+    	# from request.data return data based on time
+    	return {'data': 'happy'} 
 
 
 api.add_resource(TimeRange, '/TimeRange')
