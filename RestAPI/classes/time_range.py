29da9173d1184b85ce5271b95dd7cb955e8e6df4
K-Dasu <keshavdasu@yahoo.com>
2017-11-16 18:32:03 -0800

    Using python 3
    Migrated to use python 3 (only way to use R)

--- RestAPI/classes/time_range.py
+++ RestAPI/classes/time_range.py
@@ -3,11 +3,9 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
-from jarvis_march import JarvisMarch
+from .jarvis_march import JarvisMarch
 from api import mongo
 
-
-import requests
 import json
 
 
